962855ec882ea180238c06e76e7ca38a986662b4
IlKaiser <samuele.rinaldi90@gmail.com>
2020-10-13 17:25:48 +0200

    Fix plot

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 timestamp = []
 acc_x, acc_y, acc_z = [], [], []
@@ -13,17 +14,18 @@ with open('info.csv') as csvfile:
         if(row != []):
             if(row[0] == 'C-Filter'):
                 flag = 1
+                continue
             if(flag == 0):
                 timestamp.append(row[0])
-                acc_x.append(row[1])
-                acc_y.append(row[2])
-                acc_z.append(row[3])
-                gyro_x.append(row[4])
-                gyro_y.append(row[5])
-                gyro_z.append(row[6])
+                acc_x.append(float(row[1]))
+                acc_y.append(float(row[2]))
+                acc_z.append(float(row[3]))
+                gyro_x.append(float(row[4]))
+                gyro_y.append(float(row[5]))
+                gyro_z.append(float(row[6]))
             else:
-                pitch.append(row[0])
-                roll.append(row[1])
+                pitch.append(float(row[0]))
+                roll.append(float(row[1]))
 
 fig, axs = plt.subplots(3)
 
@@ -31,11 +33,11 @@ axs[0].plot(acc_x, label = 'x-line', linewidth = 1)
 axs[0].plot(acc_y, label = 'y-line', linewidth = 1)
 axs[0].plot(acc_z, label = 'z-line', linewidth = 1)
 
-axs[1].plot(gyro_x, label = 'y-line', linewidth = 1)
-axs[1].plot(gyro_y, label = 'x-line', linewidth = 1)
+axs[1].plot(gyro_x, label = 'x-line', linewidth = 1)
+axs[1].plot(gyro_y, label = 'y-line', linewidth = 1)
 axs[1].plot(gyro_z, label = 'z-line', linewidth = 1)
 
 axs[2].plot(pitch, label = 'pitch', linewidth = 1)
-axs[2].plot(roll, label = 'pitch', linewidth = 1)
+axs[2].plot(roll, label = 'roll', linewidth = 1)
 
 plt.show()
