3b9c46d424989b8c34e8a555bb0eedf5fbb4a450
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2020-11-08 21:04:58 +0100

    Plot only Z-axis

--- RuuviTag/plot.py
+++ RuuviTag/plot.py
@@ -9,13 +9,13 @@ with open('data.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter = ' ')
     for row in reader:
         if(row != []):
-            acc_x.append(float(row[0]))
-            acc_y.append(float(row[1]))
+            #acc_x.append(float(row[0]))
+            #acc_y.append(float(row[1]))
             acc_z.append(float(row[2]))
 
 plt.title("Acceleration")
-plt.plot(acc_x, label = 'x-axis', linewidth = 1)
-plt.plot(acc_y, label = 'y-axis', linewidth = 1)
+#plt.plot(acc_x, label = 'x-axis', linewidth = 1)
+#plt.plot(acc_y, label = 'y-axis', linewidth = 1)
 plt.plot(acc_z, label = 'z-axis', linewidth = 1)
 plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%d g'))
 plt.legend()
