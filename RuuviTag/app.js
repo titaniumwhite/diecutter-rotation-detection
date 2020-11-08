152cf061f3358b15268e479445ee813f6373da7d
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2020-11-08 21:04:36 +0100

    Set accel range to 8G

--- RuuviTag/app.js
+++ RuuviTag/app.js
@@ -1,17 +1,29 @@
-const ACC_SENSITIVITY =  1023;
+const ACC_SENSITIVITY =  222.75; //8G
+var counter = 0;
 var Ruuvitag = require("Ruuvitag");
 function startAdv(){
   Ruuvitag.setAccelOn(true, function(xyz) {
-  var d = [
-      "A",
-      xyz.x/ACC_SENSITIVITY,
-      xyz.y/ACC_SENSITIVITY,
-      xyz.z/ACC_SENSITIVITY
-  ];
-  //console.log(d.join(","));
-  Bluetooth.println(d.join(","));
+    if(counter<3000){
+      var d = [
+        "A",
+        xyz.x/ACC_SENSITIVITY,
+        xyz.y/ACC_SENSITIVITY,
+        xyz.z/ACC_SENSITIVITY,
+      ];
+      //print(d.join(","));eravamo
+      Bluetooth.println(d.join(","));
+      counter++;
+    }
+    else{
+      Ruuvitag.setAccelOn(false);
+      var f = ["F"];
+      Bluetooth.println(f);
+    }
   });
-          Ruuvitag.accel.setPowerMode("normal");
+  //100hz
+  Ruuvitag.accel.setPowerMode("normal");
+  //Set 8G range
+  Ruuvitag.accel.w(0x23,0x20);
 }
 setWatch( function() {
 Ruuvitag.setAccelOn(false);
