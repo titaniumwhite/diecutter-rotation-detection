ea74fd47e994caee6e53bf8c83a93449870c7663
Gabriele De Santis <gabriele1.desantis@gmail.com>
2020-11-24 23:47:35 +0100

    fixing minor problems

--- Force sensing resistor/app.js
+++ Force sensing resistor/app.js
@@ -1,18 +1,29 @@
-//ADC pin used is 31
+const ADC = 31;
 const sensitivity = 0.9;
 var Ruuvitag = require("Ruuvitag");
 var rotation = 0;
+var flag = 0;
 
-var idInterval = setInterval(function(){
-  pinMode(31, "analog");
-  if(analogRead(31) < sensitivity) {
-    rotation++;
-    console.log("One more rotation --> " + rotation);
-  }
-}, 50);
+function startAdv(){
+  console.log("PRESSURE SENSOR READY");
+  var idInterval = setInterval(function(){
+    pinMode(ADC, "analog");
+    var analog = analogRead(ADC);
+    //console.log(analog);
+    if(analog < sensitivity && flag == 0) {
+      rotation++;
+      flag = 1;
+      var d = ["A", analog, rotation];
+      Bluetooth.print("\n" + d.join(","));
+    } else if (analog > sensitivity && flag == 1) {
+        flag = 0;
+    }
+  }, 50);
 
-setWatch( function() {
-  clearInterval(idInterval);
-  var f = ["F"];
-  Bluetooth.println(f);
-}, BTN1, {repeat: true});
+  setWatch(function() {
+    console.log("PRESSING THE BUTTON");
+    clearInterval(idInterval);
+    rotation = 0;
+    Bluetooth.print(["F"]);
+  }, BTN1, {repeat: true});
+}
