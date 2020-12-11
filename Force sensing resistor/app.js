a74eeae81cfa7674704e1da9632e0db3b47956c2
Francesco Leotta <francescoleotta@hotmail.com>
2020-12-11 15:52:47 +0100

    New version of the pressor sensor working on moving average

--- Force sensing resistor/app.js
+++ Force sensing resistor/app.js
@@ -1,22 +1,30 @@
 const ADC = 31;
-const sensitivity = 0.9;
+const fallCorrectionFactor = 0.9;
+const riseCorrectionFactor = 1.05;
 var Ruuvitag = require("Ruuvitag");
 var rotation = 0;
 var flag = 0;
 var idInterval;
+var runningAverage = null;
+const learningFactor = 0.1;
 
 function startAdv(){
   console.log("PRESSURE SENSOR READY");
   idInterval = setInterval(function(){
     pinMode(ADC, "analog");
     var analog = analogRead(ADC);
+    if (runningAverage == null) {
+      runningAverage = analog;
+    } else {
+      runningAverage = (1-learningFactor)*runningAverage + learningFactor * analog;
+    }
     //console.log(analog);
-    if(analog < sensitivity && flag == 0) {
+    if(analog < runningAverage * fallCorrectionFactor && flag == 0) {
       rotation++;
       flag = 1;
       var d = ["A", analog, rotation];
       Bluetooth.print("\n" + d.join(","));
-    } else if (analog > sensitivity && flag == 1) {
+    } else if (analog > runningAverage * riseCorrectionFactor && flag == 1) {
         flag = 0;
     }
   }, 50);
