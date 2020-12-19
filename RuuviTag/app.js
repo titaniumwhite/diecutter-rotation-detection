5ee804c0cf859241e06acfc1f3510ca98f09833b
titaniumwhite <gabriele1.desantis@gmail.com>
2020-12-19 21:21:06 +0100

    Fixing undefined state after ruuvi acquisition

--- RuuviTag/app.js
+++ RuuviTag/app.js
@@ -1,25 +1,16 @@
 //function LIS2DH12_FROM_FS_8g_NM_TO_mg(lsb){return (float)((int16_t)lsb>>6)* 16.0f}
 const ACC_SENSITIVITY =  128; //16G
-var counter = 0;
 var Ruuvitag = require("Ruuvitag");
 function startAdv(){
   Ruuvitag.setAccelOn(true, function(xyz) {
-    if(counter<3000){
       var d = [
         "A",
         xyz.x/ACC_SENSITIVITY,
         xyz.y/ACC_SENSITIVITY,
         xyz.z/ACC_SENSITIVITY,
       ];
-      //print(d.join(","));eravamo
+      //print(d.join(","));
       Bluetooth.println(d.join(","));
-      counter++;
-    }
-    else{
-      Ruuvitag.setAccelOn(false);
-      var f = ["F"];
-      Bluetooth.println(f);
-    }
   });
   //100hz @12bit
   Ruuvitag.accel.setPowerMode("highres");
